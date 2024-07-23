3f8b5c2f0c814896587d7aa02b03f9c6283fb1fc
miinjoo <miinjoo@hyundai.com>
2024-07-23 17:30:21 +0900

    feat(feature/3):기본 로그인 form 구성

---
+++ src/app/accounts/signin/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import LoginContainer from '../../container/LoginContainer';
+
+function LoginPage() {
+  return <LoginContainer />;
+}
+
+export default LoginPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,16 @@
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <html lang="en"></html>;
+  return (
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
 }
